523c5257dfc86a716556cf68a7e1ca1e9a0aa3f4
Louis-XIV-bis <tigerwhite27@hotmail.fr>
2024-05-15 13:19:43 +0200

    plots heterozygozity

--- heteroz/run_heteroz.sh
+++ heteroz/run_heteroz.sh
@@ -4,12 +4,12 @@
 #SBATCH --mem 250GB
 #SBATCH --partition long
 
-source activate /shared/ifbstor1/projects/yeast_neutral_model/envs
+conda activate /shared/ifbstor1/projects/yeast_neutral_model/envs/
 
-# CAREFUL: give vcf file w/o '.gz' in the path, it will add it afterwards
-vcf='/shared/projects/yeast_neutral_model/vcf/2330strains_SNPs_filteredQD10_PASS_repeatMaskerGffJubin.vcf'
+vcf_path='/shared/projects/yeast_neutral_model/vcf/2330strains_SNPs_filteredQD10_PASS_repeatMaskerGffJubin.vcf.gz'
+vcf_='vcf_fanny.vcf'
 
 # Be careful: you should not give .gz to the python script
-gunzip -c $vcf.gz > ./$vcf 
+gunzip -c $vcf.gz > $vcf 
 python3 heteroz.py $vcf
-rm ./$vcf
+rm $vcf
